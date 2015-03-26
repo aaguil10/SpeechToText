400d0bf818a879ff07e13ecb698a035c35d76e1a
aaguil10 <aaguil10@ucsc.edu>
2015-03-25 18:19:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/aguilar/speechtotext/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.aguilar.speechtotext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
+
+    private EditText metTextHint;
+    private ListView mlvTextMatches;
+    private Spinner msTextMatches;
+    private Button mbtSpeak;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+                .getPackage().getName());
+
+        // Given an hint to the recognizer about what the user is going to say
+        //There are two form of language model available
+        //1.LANGUAGE_MODEL_WEB_SEARCH : For short phrases
+        //2.LANGUAGE_MODEL_FREE_FORM  : If not sure about the words or phrases and its domain.
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        Toast.makeText(this, "I'm Searching...",Toast.LENGTH_SHORT).show();
+
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+            //If Voice recognition is successful then it returns RESULT_OK
+            if(resultCode == RESULT_OK) {
+                ArrayList<String> textMatchList = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                EditText et = (EditText) this.findViewById(R.id.result_et);
+                et.setText(textMatchList.get(0));
+                //Result code for various error.
+            }else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+                showToastMessage("Audio Error");
+            }else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+                showToastMessage("Client Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+                showToastMessage("Network Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+                showToastMessage("No Match");
+            }else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+                showToastMessage("Server Error");
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /**
+     * Helper method to show the toast message
+     **/
+    void showToastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
